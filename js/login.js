8549d991cc2c7ecafcfd7d9ad79e388ce0dc62d0
marod95 <marod3995@gmail.com>
2022-08-18 17:05:29 -0300

    ent1

---
+++ js/login.js
@@ -0,0 +1,23 @@
+document.getElementById("Regresa").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+function showAlertSuccess() {
+  document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+  document.getElementById("alert-danger").classList.add("show");
+}
+function comprobarClave() {
+  Password = document.getElementById("Password").value;
+  email = document.getElementById("email").value;
+  check = document.getElementById("check").checked;
+
+  if (Password.length > 0 && email.length > 0 && check == true) {
+    showAlertSuccess();
+    window.location.href = "index.html";
+  } else {
+    showAlertError();
+  }
+}
---
+++ js/products.js
@@ -0,0 +1,45 @@
+const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+
+let productsArray = [];
+
+
+function showProductsList(array){
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){ 
+        let products = array[i];
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">                    
+                        <h4>`+ products.name +`</h4> 
+                        <p> `+ products.description +`</p> 
+                        </div>
+                        <small class="text-muted">` + products.soldCount + ` artículos</small> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+    }
+}
+    
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + EXT_TYPE).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentCategoriesArray = resultObj.data.products
+            showProductsList(currentCategoriesArray)
+            //sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+        }
+    });
+})
+
+
